3f079d80f76641e844f72c1df3c8ff110a625d7b
Jonathan dos Santos Stelle <106595353+jonathanstelle@users.noreply.github.com>
2024-11-27 10:50:00 -0300

    eeeee

--- perguntas.js
+++ perguntas.js
@@ -4,3 +4,45 @@ criaCartao(
     'Respiração'
 )
 
+criaCartao(
+    'Charadas',
+    'Quanto mais você tira maior fica?',
+    'Buraco'
+)
+criaCartao(
+    'Charadas',
+    'Quanto mais você tem, menos você vê?',
+    'Escuridão'
+)
+criaCartao(
+    'Charadas',
+    'O que corre mais não tem pernas?',
+    'Rio'
+)
+criaCartao(
+    'Charadas',
+    'O que é facil de subir mas dificil de descer?',
+    'Idade'
+)
+criaCartao(
+    'Charadas',
+    'O que tem uma perna só e um chapéu?',
+    'Cogumelo'
+)
+criaCartao(
+    'Charadas',
+    'O que é que sempre está na sua frente mas você nunca vê?',
+    'O futuro'
+)
+criaCartao(
+    'Charadas',
+    'O que é feito para andar, mas nunca sai do lugar?',
+    'A estrada'
+)
+criaCartao(
+    'Charadas',
+    'O que anda com os pés na cabeça?',
+    'O piolho'
+)
+
+
